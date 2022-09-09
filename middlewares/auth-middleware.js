e27cc95789a85ee0591004face2c801db1cf32de
mehediPC <mehedi609@gmail.com>
2022-09-09 23:24:01 +0600

    Implemented auth middleware

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,41 @@
+const expressAsyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const { User } = require('../model');
+const { UnauthorizedError } = require('../errors');
+const logger = require('../logger');
+
+const authMiddleware = expressAsyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req?.headers?.authorization?.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+        //find the user by id
+        const user = await User.findById(decoded?.id).select('-password');
+
+        //attach the user to the request object
+        req.user = {
+          id: user.id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        };
+        next();
+      } else {
+        next(new UnauthorizedError('There is no token attached to the header'));
+      }
+    } catch (error) {
+      logger.error(error);
+      next(
+        new UnauthorizedError(
+          'Not authorized! token invalid or expired, login again',
+        ),
+      );
+    }
+  }
+});
+
+module.exports = authMiddleware;
